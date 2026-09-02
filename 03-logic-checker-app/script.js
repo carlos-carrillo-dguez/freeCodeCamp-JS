9bcf171252ff5f07163179b358d4f2405b65cc48
carlos-carrillo-dguez <carlos.carrillo.dguez@gmail.com>
2026-09-02 18:21:48 +0200

    Initial commit: Add FreeCodeCamp JS labs

--- 03-logic-checker-app/script.js
+++ 03-logic-checker-app/script.js
@@ -2,21 +2,27 @@
 const hasDeveloperJob = true;
 
 if (hasDeveloperJob) {
-  console.log("Timmy is employed as a developer.");
+  console.log("Timmy is employed as a developer."); // Output: Timmy is employed as a developer.
 }
 
 // Declaracion de la Variable isTimmyAGamer y su condicion
 const isTimmyAGamer = false;
 
 if (isTimmyAGamer) {
-  console.log("Timmy loves to play World of Warcraft.");
+  console.log("Timmy loves to play World of Warcraft."); // No imprime nada porque la condición es false
 }
 
-// Declaracion de la Variable timmyAge (cambiada a un numero menor a 16) y su condicional if...else
+// Declaracion de la Variable timmyAge y su condicional if...else
 const timmyAge = 18;
 
 if (timmyAge >= 16) {
-  console.log("Timmy is old enough to drive.");
+  console.log("Timmy is old enough to drive."); // Output: Timmy is old enough to drive.
 } else {
-  console.log("Timmy is not old enough to drive.");
+  console.log("Timmy is not old enough to drive."); // No se ejecuta porque timmyAge >= 16 es true
 }
+
+/*
+  Salida en terminal
+    Timmy is employed as a developer.
+    Timmy is old enough to drive.
+*/
