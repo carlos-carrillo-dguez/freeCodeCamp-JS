29e117f8aca7742765d4224aeb03a1f0a03aaded
carlos-carrillo-dguez <carlos.carrillo.dguez@gmail.com>
2026-09-02 20:00:53 +0200

    lab conteo de cartas

--- 11-lab-conteo-cartas/script.js
+++ 11-lab-conteo-cartas/script.js
@@ -1,4 +1,14 @@
+// Variable Global para mantener el estado del contado
 let count = 0;
+/* 
+    Primer if/ else if
+        if => Cartas entre (2 al 6) e incrementar la variable 'count'
+        else if => Cartas entre (10, J, Q, K, A) y decrementar la variable 'count'
+        Cartas neutrales entre (7, 8, 9) no alteran la variable 'count'
+
+    Segundo if/ else
+        Evaluación de la decisión final según el valor actual de la variable 'count'
+ */
 function cardCounter(card){
     if (card >= 2 && card <= 6){
         count++;
