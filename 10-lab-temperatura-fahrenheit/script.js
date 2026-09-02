bffbee30f8393ef71a298bf976d502d80969d566
carlos-carrillo-dguez <carlos.carrillo.dguez@gmail.com>
2026-09-02 15:16:11 +0200

    laboratorio de pasar la temperatura a Fahrenheit

---
+++ 10-lab-temperatura-fahrenheit/script.js
@@ -0,0 +1,18 @@
+/* 
+   - Parametro => "num" --> es la representacion de la temperatura en ºC
+   - Retorno: Numero correspondiente a la temperatura en ºF
+   - Formula aplicada: Fahrenheit = Celsius * (9/5) + 32
+
+*/
+
+function convertCtoF(num){
+    return num *(9/5) + 32;
+}
+
+// Prueba de ejecucion
+console.log(convertCtoF(-30)); //22
+console.log(convertCtoF(-10)); //14
+console.log(convertCtoF(-30)); //22
+console.log(convertCtoF(0)); //32
+console.log(convertCtoF(20)); //68
+console.log(convertCtoF(30)); //86
