34b285d76d3353a807ee9dc4f99e3027aac0cdee
carlos-carrillo-dguez <carlos.carrillo.dguez@gmail.com>
2026-09-02 18:13:39 +0200

    Initial commit: Add FreeCodeCamp JS labs

--- 02-lab-fun-fact-bot/script.js
+++ 02-lab-fun-fact-bot/script.js
@@ -8,7 +8,8 @@ let noun2 = "cat";
 
 let firstStory = "Once upon a time, there was a(n)" + " " + adjective + " " + noun + " " + "who loved to eat" + " " + noun2 + "." + " " + "The" + " " + noun + " " + "lived in a" + " " + place + " " + "and had" + " " + adjective2 + " " + "nostrils that blew fire when it was" + " " + verb + ".";
 
-console.log("First story:" + " " + firstStory);
+console.log("First story:" + " " + firstStory); 
+// First story: Once upon a time, there was a(n) happy dog who loved to eat cat. The dog lived in a park and had big nostrils that blew fire when it was run.
 
 // Segunda historia (reasignando nuevos valores)
 adjective = "blue";
@@ -20,4 +21,5 @@ noun2 = "pizza";
 
 let secondStory = "Once upon a time, there was a(n)" + " " + adjective + " " + noun + " " + "who loved to eat" + " " + noun2 + "." + " " + "The" + " " + noun + " " + "lived in a" + " " + place + " " + "and had" + " " + adjective2 + " " + "nostrils that blew fire when it was" + " " + verb + ".";
 
-console.log("Second story:" + " " + secondStory);   
+console.log("Second story:" + " " + secondStory);
+// Second story: Once upon a time, there was a(n) blue car who loved to eat pizza. The car lived in a kitchen and had fast nostrils that blew fire when it was eat.
