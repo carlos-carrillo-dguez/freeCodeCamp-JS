3ed9b80f9b6daf4656d91011476f0ec1cb4c35c1
carlos-carrillo-dguez <carlos.carrillo.dguez@gmail.com>
2026-09-02 18:10:00 +0200

    Initial commit: Add FreeCodeCamp JS labs

--- 01-lab-sentence-maker/script.js
+++ 01-lab-sentence-maker/script.js
@@ -1,5 +1,5 @@
 // Mensaje de saludo
-console.log ("Hello! I'm your coding fun fact guide!");
+console.log ("Hello! I'm your coding fun fact guide!"); // Hello! I'm your coding fun fact guide!
 
 // Creacion de variables principales
 let botName = "Carlos";
@@ -7,21 +7,21 @@ let botLocation = "Spain";
 let favoriteLanguage = "JavaScript";
 
 // Mensaje en pantalla sobre el nombre, ubicacion y lenguaje favorito
-console.log ("My name is " + botName + " and I live on " + botLocation + ".");
-console.log("My favorite programming language is " + favoriteLanguage + ".");
+console.log ("My name is " + botName + " and I live on " + botLocation + "."); // My name is Carlos and I live on Spain.
+console.log("My favorite programming language is " + favoriteLanguage + "."); // My favorite programming language is JavaScript.
 
-// Declaracion nueva variable y su mensaje en pantalla
+// Declaracion nueva variable y la primera sobreescritura del mensaje
 let codingFact = "PHP";
-codingFact = "The most popular programming language is " +  favoriteLanguage + ".";
-console.log(codingFact);
+codingFact = "The most popular programming language is " +  favoriteLanguage + "."; // Sobreescribe "PHP" antes de imprimir
+console.log(codingFact); // The most popular programming language is JavaScript.
 
-// Reasignacion de la variable codigFact y su mensaje
+// Primera reasignacion de la variable codingFact y su mensaje
 codingFact = "Language new " + favoriteLanguage + ".";
-console.log(codingFact);
+console.log(codingFact); // Language new JavaScript.
 
 // Segunda reasignacion de la variable codigFact y su mensaje
 codingFact = "Uncommonly used programming language " + favoriteLanguage + ".";
-console.log(codingFact);
+console.log(codingFact); // Uncommonly used programming language JavaScript.
 
 // Mensaje de despedida
-console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + "."); 
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + "."); // It was fun sharing these facts with you. Goodbye! - Carlos from Spain.
